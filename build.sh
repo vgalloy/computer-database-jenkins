6e82ce976acb4b986e5d452cd9becbeb0d3bc853
Vincent Galloy <vgalloy@excilys.com>
2016-02-25 18:02:16 +0100

    import config

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t cdb-jenkins-image .
+docker build -t computer-database-jenkins .
---
+++ script.sh
@@ -0,0 +1,20 @@
+if [ -e $(docker ps -aq -f "name=computer-database-mysql-test") ]
+then 
+	docker create --name=computer-database-mysql-test -e MYSQL_ROOT_PASSWORD=root vgalloy/computer-database-mysql-test
+fi
+docker start computer-database-mysql-test
+sleep 3
+if [ -e $(docker ps -aq -f "name=computer-database-builder") ]
+then 
+	docker create --name=computer-database-builder --link computer-database-mysql-test:localhost -w /opt/cdb maven:3.3.3-jdk-8 mvn clean package
+fi
+docker cp . computer-database-builder:/opt/cdb
+docker start -a computer-database-builder
+docker stop computer-database-mysql-test
+
+docker cp computer-database-builder:/opt/cdb/webapp/target/webapp-1.0-RELEASE.war webapp.war
+docker build -t vgalloy/computer-database-webapp .
+docker push vgalloy/computer-database-webapp
+
+bash glazer-deploy.sh --host 192.168.10.39 --port 65000 --env MYSQL_ROOT_PASSWORD=root vgalloy-computer-database-mysql vgalloy/computer-database-mysql-prod
+bash glazer-deploy.sh --host 192.168.10.39 --port 65000 --link vgalloy-computer-database-mysql:localhost --publish 65100:8080 vgalloy-computer-database-webapp vgalloy/computer-database-webapp
